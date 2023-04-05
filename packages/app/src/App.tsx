7e6b8693549fb3cd9a2169815a36957a6855c110
EL KHALKI MOHAMED <melkhalki.soprasteria@lfdj.com>
2023-04-05 08:23:14 +0200

    update

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Body } from "lib1";
+import { Body } from "@test/lib1";
 
 function App() {
   return (
--- packages/lib1/src/components/Body.tsx
+++ packages/lib1/src/components/Body.tsx
@@ -1,9 +1,13 @@
-import * as React from 'react'
+import * as React from 'react';
 
 interface Props {
   text: string
 }
 
 export const Body = ({ text }: Props) => {
-  return <div className='body' style={{color: '#FFF'}}>Example Component change: {text}</div>
+  return (
+    <div className='body' style={{ color: "#FFF" }}>
+      Example Component : {text}
+    </div>
+  )
 }
